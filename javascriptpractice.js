f14c062b2b761e981b7ee33de757e155850af830
Prem_Saragada <149216070+PremReddy-585@users.noreply.github.com>
2024-01-13 22:34:03 +0530

    Create javascriptpractice.js

---
+++ javascriptpractice.js
@@ -0,0 +1,8 @@
+// creating an Array
+console.log(["Orange", 25, 100, true, 33.58]) 
+
+//Acessing An array
+  let myArray = [25, false, "green", "apple"];
+  console.log(myArray[index]);
+
+//update the values of Array
