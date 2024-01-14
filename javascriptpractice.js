0415b2269a96504e247438f4af5ae949743a2188
Prem_Saragada <149216070+PremReddy-585@users.noreply.github.com>
2024-01-14 19:32:32 +0530

    Update javascriptpractice.js

--- javascriptpractice.js
+++ javascriptpractice.js
@@ -1,8 +1,22 @@
-// creating an Array
-console.log(["Orange", 25, 100, true, 33.58]) 
+//creating and log an array
+let myArray=['orange', 22, 333, true, false, 88.99];
+console.log(myArray);
 
-//Acessing An array
-  let myArray = [25, false, "green", "apple"];
-  console.log(myArray[index]);
+//Access the values of an any array 
+console.log(myArray[2]);
 
-//update the values of Array
+//update the values of an array
+myArray[1] = 2299
+console.log(myArray[1]);
+
+//finding the length of an array
+let length = myArray.length
+console.log(length);
+
+//Adding an value at the end of the array
+myArray.push(19);
+console.log(myArray);
+
+//deleting an array form an array
+myArray.pop(4);
+console.log(myArray);
